54e742bc1bb70918ee6602ecdb381b1ffda24423
hygxsh2k <hygxsh2k@gmail.com>
2022-02-12 13:44:10 +0900

    fix server-list

--- 02-server-list.js
+++ 02-server-list.js
@@ -1,5 +1,5 @@
 export async function main(ns) {
   let servers = {};
-  ns.scan("home").forEach(server => hash[server] = 1);
+  ns.scan("home").forEach(server => servers[server] = 1);
   ns.tprint(servers);
 }
